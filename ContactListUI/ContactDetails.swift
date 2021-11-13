069be06b6c0e651b9807de0f01a1a6c904db1655
aizaomar <w.omaraiza@gmail.com>
2021-11-13 19:38:38 +0600

    Files

---
+++ ContactListUI/ContactDetails.swift
@@ -0,0 +1,20 @@
+//
+//  ContactDetails.swift
+//  ContactListUI
+//
+//  Created by Aiza on 13.11.2021.
+//
+
+import SwiftUI
+
+struct ContactDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactDetails()
+    }
+}
---
+++ ContactListUI/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  ContactListUI
+//
+//  Created by Aiza on 13.11.2021.
+//
+
+import SwiftUI
+
+struct Person: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Person_Previews: PreviewProvider {
+    static var previews: some View {
+        Person()
+    }
+}
