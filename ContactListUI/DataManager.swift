563bc6a1d93952f1a491a7fcfec672c739a7cd08
aizaomar <w.omaraiza@gmail.com>
2021-11-13 19:59:38 +0600

    Data Manager

--- ContactListUI/DataManager.swift
+++ ContactListUI/DataManager.swift
@@ -5,16 +5,40 @@
 //  Created by Aiza on 13.11.2021.
 //
 
-import SwiftUI
-
-struct DataManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct DataManager_Previews: PreviewProvider {
-    static var previews: some View {
-        DataManager()
-    }
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let names = [
+        "Valentina", "Alessandro", "Gervasio",
+        "Maria", "Cuoco", "Manlio", "Delfina",
+        "Oreste", "De Luca", "Maria", "Livio",
+        "Costanza", "Manco", "Anna", "Oscarr",
+    ]
+    
+    let surnames = [
+        "Fiore", "Rufino", "Attillio",
+        "Lando", "Emilio", "Antiocco",
+        "Aloisi", "Filibero", "Agata",
+        "Oscar", "Oddon", "D'Antonio",
+        "Roberto", "Flaviano", "Gaaf"
+    ]
+    
+    let phoneNumbers = [
+        "7009812120", "7705678692", "7989636360",
+        "7000876345", "7008294506", "7001238788",
+        "7770002311", "7805674320", "7001236733",
+        "7717652390", "7001235755", "7001236777",
+        "7005674844", "7770001122", "7009993321",
+    ]
+    
+    let emails = [
+        "qrt@gmail.com", "jhg@gmail.com", "nbh@gmail.com",
+        "zxc@gmail.com", "pbb@gmail.com", "bfy@gmail.com",
+        "pjh@gmail.com", "dff@gmail.com", "sdf@gmail.com",
+        "oiu@gmail.com", "iuy@gmail.com", "vvv@gmail.com",
+        "trc@gmail.com", "qaz@gmail.com", "mmm@gmail.com",
+    ]
+    
+    private init() {}
 }
