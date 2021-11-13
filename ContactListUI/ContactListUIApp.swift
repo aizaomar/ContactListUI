d768018d9ec664ed62b507f24353cf13e3b84097
aizaomar <w.omaraiza@gmail.com>
2021-11-13 17:08:44 +0600

    Initial Commit

---
+++ ContactListUI/ContactListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUIApp.swift
+//  ContactListUI
+//
+//  Created by Aiza on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
